1767d03417b5b78cb2c985e7a870e10250a24c11
Rustem Davletshin <rustemd02@mail.ru>
2021-03-19 23:53:42 +0300

    Second commit

--- src/book_store.cpp
+++ src/book_store.cpp
@@ -9,12 +9,18 @@ ResizeStorageStatus resize_storage(Book *&storage, int size, int new_capacity) {
     // Tip 1: проведите валидацию аргументов функции
     // Tip 2: не забудьте высвободить ранее выделенную память под хранилище
 
-    if (*&storage == nullptr){
+    if (storage == nullptr) {
         return ResizeStorageStatus::NULL_STORAGE;
     }
+    if (size<0){
+        return ResizeStorageStatus::NEGATIVE_SIZE;
+    }
 
-    if (new_capacity > size){
-
+    if (new_capacity > size) {
+        auto *new_capacity_ = new Book[new_capacity];
+        std::copy(storage, storage + size, new_capacity_);
+        delete[] storage;
+        storage = new_capacity_;
         return ResizeStorageStatus::SUCCESS;
     } else {
         return ResizeStorageStatus::INSUFFICIENT_CAPACITY;
@@ -23,60 +29,67 @@ ResizeStorageStatus resize_storage(Book *&storage, int size, int new_capacity) {
 
 // 2. реализуйте конструктор ...
 BookStore::BookStore(const std::string &name) : name_{name} {
-  // валидация аргумента
-  if (name.empty()) {
-    throw std::invalid_argument("BookStore::name must not be empty");
-  }
+    // валидация аргумента
+    if (name.empty()) {
+        throw std::invalid_argument("BookStore::name must not be empty");
+    }
     name_ = name;
-    storage_capacity_ = 0;
+    storage_capacity_ = kInitStorageCapacity;
+    storage_ = new Book[storage_capacity_];
 
-
-  // здесь мог бы быть ваш сотрясающий землю и выделяющий память код ...
+    // здесь мог бы быть ваш сотрясающий землю и выделяющий память код ...
 }
 
 // 3. реализуйте деструктор ...
 BookStore::~BookStore() {
 
-  // здесь мог бы быть ваш высвобождающий разум от негатива код ...
-  // Tip 1: я свободен ..., словно память в куче: не забудьте обнулить указатель
+
+    // здесь мог бы быть ваш высвобождающий разум от негатива код ...
+    // Tip 1: я свободен ..., словно память в куче: не забудьте обнулить указатель
+
+    storage_size_ = 0;
+    storage_capacity_ = 0;
+    storage_ = nullptr;
+    delete[] storage_;
+
 }
 
 // 4. реализуйте метод ...
 void BookStore::AddBook(const Book &book) {
-  if (storage_size_ == storage_capacity_) {
-    // здесь мог бы быть ваш умопомрачительный код ...
-    // Tip 1: используйте функцию resize_storage_internal, задав новый размер хранилища
-    // Tip 2: не забудьте обработать статус вызова функции
-  }
-  // Tip 3: не забудьте добавить книгу в наше бездонное хранилище ...
+    if (storage_size_ == storage_capacity_) {
+        // здесь мог бы быть ваш умопомрачительный код ...
+        // Tip 1: используйте функцию resize_storage_internal, задав новый размер хранилища
+        // Tip 2: не забудьте обработать статус вызова функции
+    }
+    // Tip 3: не забудьте добавить книгу в наше бездонное хранилище ...
 }
 
 // РЕАЛИЗОВАНО
 
 const std::string &BookStore::GetName() const {
-  return name_;
+    return name_;
 }
 
 int BookStore::GetSize() const {
-  return storage_size_;
+    return storage_size_;
 }
 
 int BookStore::GetCapacity() const {
-  return storage_capacity_;
+    return storage_capacity_;
 }
 
 const Book *BookStore::GetBooks() const {
-  return storage_;
+    return storage_;
 }
 
 ResizeStorageStatus BookStore::resize_storage_internal(int new_capacity) {
-  // изменяем размеры хранилища с копированием старых данных в хранилище нового объема
-  const ResizeStorageStatus status = resize_storage(storage_, storage_size_, new_capacity);
+    // изменяем размеры хранилища с копированием старых данных в хранилище нового объема
+    const ResizeStorageStatus status = resize_storage(storage_, storage_size_, new_capacity);
 
-  // если все прошло ОК, то сохраняем новый объем хранилища в объекте
-  if (status == ResizeStorageStatus::SUCCESS) {
-    storage_capacity_ = new_capacity;
-  }
+    // если все прошло ОК, то сохраняем новый объем хранилища в объекте
+    if (status == ResizeStorageStatus::SUCCESS) {
+        storage_capacity_ = new_capacity;
+    }
 
-  return status;
+    return status;
 }
